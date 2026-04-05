3a0aa82a3ee6cdb8dacbc5ce0ce9e207c5103883
benluiwj <wenjie.lui@gmail.com>
2026-04-05 18:38:59 +0800

    fix db

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,19 @@
+use crate::routes::{health_check, subscriptions};
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
+use sqlx::{Pool, Postgres};
 use std::net::TcpListener;
 
-use crate::routes::{health_check, subscriptions};
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(
+    listener: TcpListener,
+    connection_pool: Pool<Postgres>,
+) -> Result<Server, std::io::Error> {
+    let data_connection_pool = web::Data::new(connection_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check::health_check))
             .route("/subscriptions", web::post().to(subscriptions::subscribe))
+            .app_data(data_connection_pool.clone())
     })
     .listen(listener)?
     .run();
